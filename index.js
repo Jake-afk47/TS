8ce5e8668ef320afe938e207951ebb34d6a0b535
Paulo Vinicius <pvinicius7156@gmail.com>
2025-08-15 22:09:59 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+var fistNumber = 7;
+var secondNumber = 3;
+function addNumbers(a, b) {
+    return a + b;
+}
+console.log("primeiro numero:", fistNumber);
+console.log("segundo numero: ", secondNumber);
+console.log("resultado da soma deles: ", addNumbers(fistNumber, secondNumber));
---
+++ index.ts
@@ -0,0 +1,10 @@
+const fistNumber = 7;
+const secondNumber = 3;
+
+function addNumbers(a: number, b: number): number {
+    return a + b;
+}
+
+console.log("primeiro numero:", fistNumber);
+console.log("segundo numero: ", secondNumber);
+console.log("resultado da soma deles: ", addNumbers(fistNumber, secondNumber));

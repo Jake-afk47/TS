75eab43896456ccdfd192da6514f4ca0b0ed1c7b
Jake-afk47 <pvinicius7156@gmail.com>
2025-08-16 09:39:21 -0300

    Atualização

---
+++ 2seção/index.ts
@@ -0,0 +1,38 @@
+// Variavel number;
+
+let x: number = 7
+
+const y: number = 3.14906584
+
+console.log(x)
+console.log(typeof x)
+
+console.log(y.toPrecision(2))
+console.log(typeof y)
+
+
+
+//Variavrl string;
+
+const fistName: string = "Paulo"
+
+console.log(fistName)
+
+let fullName: string
+
+const lestName: string = "Vinicius"
+
+console.log(fullName = fistName + " " + lestName);
+
+
+
+// Variavel boolean;
+
+let u = true
+
+console.log(u)
+
+u = false
+
+console.log(u)
+console.log(typeof u)

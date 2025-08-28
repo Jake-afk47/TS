19ea738a276621c4d0fb466d239d4ec457406a18
Jake-afk47 <pvinicius7156@gmail.com>
2025-08-28 10:37:06 -0300

    Nova seção

---
+++ 3seção/index.js
@@ -0,0 +1,7 @@
+// Arrays
+var numeros = [1, 2, 3, 4];
+numeros.push(4);
+console.log(numeros[1]);
+var nomes = ["Paulo", "Erick", "Gabriel gay"];
+nomes.push("Paulo");
+console.log(nomes[2]);
---
+++ 3seção/index.ts
@@ -0,0 +1,13 @@
+// Arrays
+
+let numeros: number[] = [1, 2, 3, 4]
+
+numeros.push(4)
+
+console.log(numeros[1])
+
+let nomes: string[] = ["Paulo", "Erick", "Gabriel gay"]
+
+nomes.push("Paulo")
+
+console.log(nomes[2])
